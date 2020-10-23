2a941fbc707003f2d0270d455334df4999020ef5
David Bing <davidb.devops@gmail.com>
2020-10-23 13:50:10 -0500

    import and invoke RideTicketHolders() component

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 // Import functions from external modules
+import { RideTicketHolders } from './rides/RideTicketHolders.js';
 import { TicketBooth } from './TicketBooth.js'
 
 // invoke functions
 TicketBooth();
-
+RideTicketHolders();
 
