fc2f12cf60a569f15dadbe911412862ed2d22e53
David Bing <davidb.devops@gmail.com>
2020-10-23 12:55:49 -0500

    implement eventHub along with event listener and CustomEvent creation in response to rideTicket click event

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,12 +1,37 @@
+// get a reference to html section element with attribute class="entry"
 const contentTarget = document.querySelector(".entry")
+
+// hang evenHub on html attribute id: state-fair
 const eventHub = document.querySelector("#state-fair")
+  //debug
+  console.log(eventHub)
 
-eventHub.addEventListener()
+// event listener to capture click events and create custom event when rideTicket event is detected
+eventHub.addEventListener("click", clickEvent => {
+    // debug
+    console.log(clickEvent)
+    
+    // ensure click has clickEvent.target.id of "rideTicket"
+    if (clickEvent.target.id === "rideTicket") {
+        // create custom event to describe what happened
+        const rideTicketSelectedEvent = new CustomEvent("rideTicketSelected", {
+            detail: {
+                rideTicketSelected: clickEvent.target.value
+            }
+        })
+        // debug code
+        console.log(rideTicketSelectedEvent)
+        // dispatch rideTicketSelectedEvent event to eventHub
+        eventHub.dispatchEvent(rideTicketSelectedEvent)
+    }
+})
 
+
+// render html to section element with attribute class="entry"
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+          <button id="rideTicket">Ride Ticket</button>
         </div>
     `
 }
