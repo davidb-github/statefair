6db787b317198c9f006c98531229d69687ac9cb0
David Bing <davidb.devops@gmail.com>
2020-10-24 11:34:28 -0500

    update module: main.js - import/invoke FoodTicketHolders()

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 // Import functions from external modules
+import { FoodTicketHolders } from './food/FoodTicketHolder.js';
 import { RideTicketHolders } from './rides/RideTicketHolders.js';
-import { TicketBooth } from './TicketBooth.js'
+import { TicketBooth }       from './TicketBooth.js'
 
 // invoke functions
 TicketBooth();
 RideTicketHolders();
+FoodTicketHolders();
 
