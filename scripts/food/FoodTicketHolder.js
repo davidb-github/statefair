d0a79d1ba2d61d56168e44a43b413e98baba7fe1
David Bing <davidb.devops@gmail.com>
2020-10-24 11:31:00 -0500

    module update: FoodTicketHolder.js - implment FoodTicketHolders() w/ proper addEventListener action

---
+++ scripts/food/FoodTicketHolder.js
@@ -0,0 +1,20 @@
+// get a ref to the target element
+const contentTarget = document.querySelector(".food")
+
+// hang eventHub on top level container...main here
+const eventHub      = document.querySelector("#state-fair")
+
+// listen for customEvent: "foodTicketSelected" and respond with insertion of <div> element with proper classes
+export const FoodTicketHolders = () => {
+    // listen for event and 
+    eventHub.addEventListener("foodTicketSelected", customEvent => {
+        // debug
+        console.log(customEvent)
+        
+        // insert new <div> element inside of <div> element targeted in contentTarget
+        contentTarget.innerHTML += `
+        <div class="person eater"> </div>
+        `
+
+    })
+}
